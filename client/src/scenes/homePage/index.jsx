1dcbe03569f32ec46eb562ab41e68fb8f31726d4
Amlan Shanker <amlanshanker2005@gmail.com>
2024-06-21 11:51:43 +0530

    Created Homepage Navbar Profile Page and added DarkMode and Logout

---
+++ client/src/scenes/homePage/index.jsx
@@ -0,0 +1,27 @@
+import { Box, useMediaQuery } from "@mui/material";
+import { useSelector } from "react-redux";
+import Navbar from "scenes/navBar";
+import UserWidget from "scenes/widgets/UserWidget";
+
+const HomePage = () => {
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+  const { _id, picturePath } = useSelector((state) => state.user);
+
+  return (
+    <Box>
+      <Navbar />
+      <Box
+        width="100%"
+        padding="2rem"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="0.5rem"
+        justifyContent="space-between"
+      >
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath} />
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+export default HomePage;
